e67d20fbfe3b9661603e0831cb15ca295bc84b02
jaguar6105 <gilljoseph603@gmail.com>
2020-09-15 13:00:59 -0400

    Add javascript for main page

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 var questionHeader = document.querySelector(".question");
+var contentEl = document.querySelector(".content");
 
-questionHeader.textContent = "Coding Quiz";
+homeScreen();
+
+//opens home screen
+function homeScreen() {
+    //fill question
+    questionHeader.textContent = "Coding Quiz";
+
+
+    // create p
+    var text = document.createElement("p");
+    text.textContent = "Answer the questions within the time limit."
+    contentEl.appendChild(text);
+
+    //create button
+    var start = document.createElement("button");
+    start.className = "btn btn-primary";
+    start.textContent = "START";
+    start.addEventListener("click", startQuiz);
+    contentEl.appendChild(start);
+}
+
+function startQuiz() {
+    console.log("button pressed");
+}
